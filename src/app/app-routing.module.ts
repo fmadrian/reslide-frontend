8fbbf4bf59eb09932e231dd39c64f2725b31b317
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2021-07-28 15:24:46 -0600

    Routing added
    Routing added for the new components created.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseComponent } from './component/page/base/base.component';
+import { LoginComponent } from './component/page/login/login.component';
+import { SignupComponent } from './component/page/signup/signup.component';
+import { AppRoutes } from './utils/routes';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : AppRoutes.landing, component: BaseComponent},
+  {path: AppRoutes.signup, component: SignupComponent},
+  {path: AppRoutes.login, component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/utils/routes.ts
@@ -0,0 +1,5 @@
+export const AppRoutes = {
+    "login" : "login",
+    "signup" : "signup",
+    "landing" : ""
+}
