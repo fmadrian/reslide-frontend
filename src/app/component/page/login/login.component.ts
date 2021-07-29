6113509e30abe778557f8ae6545b906ffc42954d
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2021-07-29 11:52:33 -0600

    Login visuals implemented

--- src/app/component/page/login/login.component.ts
+++ src/app/component/page/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  hidePassword: boolean; // is the password hidden
+  error: any; // object used to represent the error on the component (if there is one)
 
-  ngOnInit(): void {
+  loginForm : FormGroup;
+
+  constructor() { 
+    this.error = {
+      active : false,
+      message : ''
+    };
+    this.hidePassword = true;
+
+    this.loginForm = new FormGroup({
+      username : new FormControl('', Validators.required),
+      password : new FormControl('', Validators.required)
+    });
   }
 
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      username : new FormControl('', Validators.required),
+      password : new FormControl('', Validators.required)
+    });
+  }
+  
+  switchPassword(){
+    this.hidePassword = !this.hidePassword;
+  }
 }
