acf90e20476f576355a783b2f086ea34d968253d
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2021-08-20 13:29:55 -0600

    Snackbar service implemented

--- src/app/component/page/individual/create-individual/create-individual.component.ts
+++ src/app/component/page/individual/create-individual/create-individual.component.ts
@@ -3,32 +3,33 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { IndividualPayload } from 'src/app/payload/individual/individual.payload';
 import { IndividualService } from 'src/app/service/individual/individual.service';
+import { SnackbarService } from 'src/app/service/snackbar/snackbar.service';
 import { ApiError, ApiErrorMessage } from 'src/app/utils/apiErrorMessages';
 
 @Component({
   selector: 'app-create-individual',
   templateUrl: './create-individual.component.html',
-  styleUrls: ['./create-individual.component.scss']
+  styleUrls: ['./create-individual.component.scss'],
 })
 export class CreateIndividualComponent implements OnInit {
-  apiError : (ApiError | null) = null;
-  constructor(private individualService: IndividualService, private _snackBar: MatSnackBar, private router: Router) { 
-    
-  }
+  apiError: ApiError | null = null;
+  constructor(
+    private individualService: IndividualService,
+    private snackbarService: SnackbarService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
-
-  }
-  createIndividual(individual : IndividualPayload){
+  ngOnInit(): void {}
+  createIndividual(individual: IndividualPayload) {
     this.individualService.create(individual).subscribe(
-      (data)=>{
-        this._snackBar.open('Individual created', 'OK', {duration: 3000});
+      (data) => {
+        this.snackbarService.show('Individual updated');
         this.apiError = null;
-      },(error)=>{
-        this.apiError = ApiErrorMessage(error.error);
-        this._snackBar.open(error.error.message, 'OK', {duration: 3000});
+      },
+      (error) => {
+        this.apiError = ApiErrorMessage(error.error); // We pass it to the child component
+        this.snackbarService.show(error.error.message);
       }
-    )
-  } 
-
+    );
+  }
 }
--- src/app/component/page/individual/update-individual/update-individual.component.ts
+++ src/app/component/page/individual/update-individual/update-individual.component.ts
@@ -3,45 +3,47 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IndividualPayload } from 'src/app/payload/individual/individual.payload';
 import { IndividualService } from 'src/app/service/individual/individual.service';
+import { SnackbarService } from 'src/app/service/snackbar/snackbar.service';
 import { ApiError, ApiErrorMessage } from 'src/app/utils/apiErrorMessages';
 
 @Component({
   selector: 'app-update-individual',
   templateUrl: './update-individual.component.html',
-  styleUrls: ['./update-individual.component.scss']
+  styleUrls: ['./update-individual.component.scss'],
 })
 export class UpdateIndividualComponent implements OnInit {
-
-  apiError : (ApiError | null) = null;
-  individualInput : (IndividualPayload | null) = null
+  apiError: ApiError | null = null;
+  individualInput: IndividualPayload | null = null;
   id = 0; // Id of the individual to be modified.
-  error : string | null = null; // Error while searching for the individual.
-  constructor(private individualService: IndividualService, private _snackBar: MatSnackBar, private router: Router, 
-    private activateRoute: ActivatedRoute) { 
-    
-  }
+  error: string | null = null; // Error while searching for the individual.
+  constructor(
+    private individualService: IndividualService,
+    private snackbarService: SnackbarService,
+    private activateRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     // 1. Get the individual using the code passed in the parameters (query parameters).
-   this.id = this.activateRoute.snapshot.params.id;
-   this.individualService.get(this.id).subscribe(
-     (data) =>{
-       this.individualInput = data;
-     },(error)=>{
-       this.error = error.error.message;
-     }
-   )
-  }
-  updateIndividual(individual : IndividualPayload){
-    this.individualService.update(individual).subscribe(
-      (data)=>{
-        this._snackBar.open('Individual updated', 'OK', {duration: 3000});
-        this.apiError = null;
-      },(error)=>{
-        this.apiError = ApiErrorMessage(error.error); // We pass it to the child component
-        this._snackBar.open(error.error.message, 'OK', {duration: 3000});
+    this.id = this.activateRoute.snapshot.params.id;
+    this.individualService.get(this.id).subscribe(
+      (data) => {
+        this.individualInput = data;
+      },
+      (error) => {
+        this.error = error.error.message;
       }
-    )
-  } 
-  
+    );
+  }
+  updateIndividual(individual: IndividualPayload) {
+    this.individualService.update(individual).subscribe(
+      (data) => {
+        this.snackbarService.show('Individual updated');
+        this.apiError = null;
+      },
+      (error) => {
+        this.apiError = ApiErrorMessage(error.error); // We pass it to the child component
+        this.snackbarService.show(error.error.message);
+      }
+    );
+  }
 }
