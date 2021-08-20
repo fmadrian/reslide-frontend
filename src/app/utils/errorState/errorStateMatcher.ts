7de0822e8f8527abafe9c6cf9296b11161adbda4
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2021-08-19 21:51:28 -0600

    ErrorState and form validation changes

--- src/app/utils/errorState/errorStateMatcher.ts
+++ src/app/utils/errorState/errorStateMatcher.ts
@@ -1,15 +1,22 @@
-import { FormControl, FormGroupDirective, NgForm } from "@angular/forms";
-import { ErrorStateMatcher } from "@angular/material/core";
+import { FormControl, FormGroupDirective, NgForm } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 export class CustomErrorStateMatcher implements ErrorStateMatcher {
-    isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-      // Indicates if a form has been submitted
-      const isSubmitted = form && form.submitted;
-      // Restrictions that could make a form input invalid.
-      // minlength isntead of minLength
-      const isNotValid = (control?.errors?.required || control?.errors?.minlength || control?.errors?.maxlength)
-      // Returns error state if any of the restrictions is not being met. Or if we try to submit it.
-      return ((control?.dirty && isNotValid) || (isNotValid && isSubmitted));
-      // return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-    }
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    // Indicates if a form has been submitted
+    const isSubmitted = form && form.submitted;
+    // Restrictions that could make a form input invalid.
+    // minlength isntead of minLength
+    const isNotValid =
+      control?.errors?.required ||
+      control?.errors?.minlength ||
+      control?.errors?.maxlength ||
+      control?.errors?.min;
+    // Returns error state if any of the restrictions is not being met. Or if we try to submit it.
+    return (control?.dirty && isNotValid) || (isNotValid && isSubmitted);
+    // return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
   }
+}
--- src/app/utils/formValidation.ts
+++ src/app/utils/formValidation.ts
@@ -1,16 +1,18 @@
-import { FormGroup } from "@angular/forms";
+import { FormGroup } from '@angular/forms';
 
 export const FormValidation = {
-    errors : ['minlength', 'required', 'maxlength'],
-    getErrorMessage : (controlName: string, form: FormGroup)=>{
-        let control = form.get(controlName)
-        if(control?.errors?.required){
-            return `Please introduce a ${controlName}`;
-        }else if(control?.errors?.minlength){
-            return `The ${controlName} can't be shorter than ${control?.errors?.minlength.requiredLength} characters`;
-        }else if(control?.errors?.maxlength){
-            return `The ${controlName} can't be longer than ${control?.errors?.maxlength.requiredLength} characters`;
-        }
-        return ''
+  errors: ['minlength', 'required', 'maxlength'],
+  getErrorMessage: (controlName: string, form: FormGroup) => {
+    let control = form.get(controlName);
+    if (control?.errors?.required) {
+      return `Please introduce a ${controlName}`;
+    } else if (control?.errors?.minlength) {
+      return `The ${controlName} can't be shorter than ${control?.errors?.minlength.requiredLength} characters`;
+    } else if (control?.errors?.maxlength) {
+      return `The ${controlName} can't be longer than ${control?.errors?.maxlength.requiredLength} characters`;
+    } else if (control?.errors?.min) {
+      return `The ${controlName} has to be more than ${control?.errors?.min.requiredLength}`;
     }
-}
+    return '';
+  },
+};
