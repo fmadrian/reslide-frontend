e4cce3b89ed25c16f563a91ec65a497465620f71
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2021-08-19 21:51:59 -0600

    Update menu.ts
    Product pages added

--- src/app/utils/menu/menu.ts
+++ src/app/utils/menu/menu.ts
@@ -1,39 +1,42 @@
-import { AppRoutes } from "../appRoutes"
-import { MenuItem } from "./menuItem";
-import { SubMenuItem } from "./subMenuItem";
+import { AppRoutes } from '../appRoutes';
+import { MenuItem } from './menuItem';
+import { SubMenuItem } from './subMenuItem';
 
-var createSubItem = (name:string, route:string, description :string = '') =>{
-    let subItem : SubMenuItem = {
-        name, route, description
-    }
-    return subItem;
-}
-var createMainItem = (name: string, subItems: SubMenuItem[])=>{
-    let item : MenuItem = {
-        name, subItems
-    }
-    return item;
-}
+var createSubItem = (name: string, route: string, description: string = '') => {
+  let subItem: SubMenuItem = {
+    name,
+    route,
+    description,
+  };
+  return subItem;
+};
+var createMainItem = (name: string, subItems: SubMenuItem[]) => {
+  let item: MenuItem = {
+    name,
+    subItems,
+  };
+  return item;
+};
 /**
- * Object used to structure the sidebar menu. 
+ * Object used to structure the sidebar menu.
  */
-export const SidenavMenu =[
-    createMainItem('Users', 
-        [
-            createSubItem('Create new user', AppRoutes.user.create, 'Creates a new user')
-        ]
+export const SidenavMenu = [
+  createMainItem('Users', [
+    createSubItem(
+      'Create new user',
+      AppRoutes.user.create,
+      'Creates a new user'
     ),
-    createMainItem('Clients / Providers',
-        [
-            createSubItem('Create new client or provider', AppRoutes.individual.create),
-            createSubItem('Search for a client or provider', AppRoutes.individual.search),
-        ]
+  ]),
+  createMainItem('Clients / Providers', [
+    createSubItem('Create new client or provider', AppRoutes.individual.create),
+    createSubItem(
+      'Search for a client or provider',
+      AppRoutes.individual.search
     ),
-    createMainItem('Products',
-        [
-            createSubItem('Create new product', AppRoutes.product.create),
-            createSubItem('Search for a product', AppRoutes.product.search),
-        ]
-    )
-    
+  ]),
+  createMainItem('Products', [
+    createSubItem('Create new product', AppRoutes.product.create),
+    createSubItem('Search for a product', AppRoutes.product.search),
+  ]),
 ];
