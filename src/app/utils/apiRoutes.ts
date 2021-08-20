557f6d0e8cb33bd3c055a9946a192192fd98a092
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2021-08-19 21:50:55 -0600

    New api and app routes added

--- src/app/utils/apiRoutes.ts
+++ src/app/utils/apiRoutes.ts
@@ -1,30 +1,58 @@
 const serverRoute = 'http://localhost:8080/api';
 const baseRoutes = {
-    auth : `${serverRoute}/auth`,
-    individual : `${serverRoute}/individual`,
-    individualType : `${serverRoute}/individual/type`,
-    contactType : `${serverRoute}/contact/type`,
-}
+  auth: `${serverRoute}/auth`,
+  individual: `${serverRoute}/individual`,
+  individualType: `${serverRoute}/individual/type`,
+  contactType: `${serverRoute}/contact/type`,
+  product: `${serverRoute}/product`,
+  measurementType: `${serverRoute}/measurement/type`,
+  productType: `${serverRoute}/product/type`,
+  productBrand: `${serverRoute}/product/brand`,
+};
 
 export const ApiRoutes = {
-    auth :{
-        createUser : `${baseRoutes.auth}/create/user`,
-        login : `${baseRoutes.auth}/login`,
-        refreshToken : `${baseRoutes.auth}/refresh/token`
-    },
-    individual : {
-        search: `${baseRoutes.individual}/search`,
-        get: `${baseRoutes.individual}/get`,
-        create: `${baseRoutes.individual}/create`,
-        update: `${baseRoutes.individual}/update`,
-    },
-    individualType : {
-        getAll: `${baseRoutes.individualType}/getAll`,
-        create: `${baseRoutes.individualType}/create`,
-        update: `${baseRoutes.individualType}/update`,
-    },
-    contactType : {
-        search: `${baseRoutes.contactType}/search`,
-        create: `${baseRoutes.contactType}/create`,
-    },
-}
+  auth: {
+    createUser: `${baseRoutes.auth}/create/user`,
+    login: `${baseRoutes.auth}/login`,
+    refreshToken: `${baseRoutes.auth}/refresh/token`,
+  },
+  individual: {
+    search: `${baseRoutes.individual}/search`,
+    get: `${baseRoutes.individual}/get`,
+    create: `${baseRoutes.individual}/create`,
+    update: `${baseRoutes.individual}/update`,
+  },
+  individualType: {
+    getAll: `${baseRoutes.individualType}/getAll`,
+    create: `${baseRoutes.individualType}/create`,
+    update: `${baseRoutes.individualType}/update`,
+  },
+  contactType: {
+    search: `${baseRoutes.contactType}/search`,
+    create: `${baseRoutes.contactType}/create`,
+  },
+  product: {
+    search: `${baseRoutes.product}/search`,
+    update: `${baseRoutes.product}/update`,
+    create: `${baseRoutes.product}/create`,
+    get: `${baseRoutes.product}/get`,
+  },
+  measurementType: {
+    search: `${baseRoutes.measurementType}/search`,
+    get: `${baseRoutes.measurementType}/get`,
+    create: `${baseRoutes.measurementType}/create`,
+    update: `${baseRoutes.measurementType}/update`,
+  },
+  productType: {
+    search: `${baseRoutes.productType}/search`,
+    get: `${baseRoutes.productType}/get`,
+    create: `${baseRoutes.productType}/create`,
+    update: `${baseRoutes.productType}/update`,
+  },
+  productBrand: {
+    search: `${baseRoutes.productBrand}/search`,
+    get: `${baseRoutes.productBrand}/get`,
+    create: `${baseRoutes.productBrand}/create`,
+    update: `${baseRoutes.productBrand}/update`,
+  },
+};
--- src/app/utils/appRoutes.ts
+++ src/app/utils/appRoutes.ts
@@ -1,26 +1,35 @@
-const baseRoutes ={
-    'user' : 'user',
-    'individual' : 'individual',
-    'product' : 'product'
-}
+const baseRoutes = {
+  user: 'user',
+  individual: 'individual',
+  product: 'product',
+  measurementType: 'measurementType',
+};
 export const AppRoutes = {
-    'login' : 'login',
-    'landing' : '',
-    'user' : {
-        'create' : `${baseRoutes.user}/create`
-    },
-    'individual' : {
-        'create' : `${baseRoutes.individual}/create`,
-        'search' : `${baseRoutes.individual}/search`,
-        'update': `${baseRoutes.individual}/update/:id`,
-        'update_id': (id:number) => `${baseRoutes.individual}/update/${id}` // Includes the id
-    },
-    'product' : {
-        'create' : `${baseRoutes.product}/create`,
-        'search' : `${baseRoutes.product}/search`
-    },
-    'error':{
-        'notFound' : '**',
-        'internal' : 'internal'
-    }
-}
+  login: 'login',
+  landing: '',
+  user: {
+    create: `${baseRoutes.user}/create`,
+  },
+  individual: {
+    create: `${baseRoutes.individual}/create`,
+    search: `${baseRoutes.individual}/search`,
+    update: `${baseRoutes.individual}/update/:id`,
+    update_id: (id: number) => `${baseRoutes.individual}/update/${id}`, // Includes the id
+  },
+  measurementType: {
+    create: `${baseRoutes.measurementType}/create`,
+    search: `${baseRoutes.measurementType}/search`,
+    update: `${baseRoutes.measurementType}/update/:id`,
+    update_id: (id: number) => `${baseRoutes.measurementType}/update/${id}`, // Includes the id
+  },
+  product: {
+    create: `${baseRoutes.product}/create`,
+    search: `${baseRoutes.product}/search`,
+    update: `${baseRoutes.product}/update/:id`,
+    update_id: (id: number) => `${baseRoutes.product}/update/${id}`, // Includes the id
+  },
+  error: {
+    notFound: '**',
+    internal: 'internal',
+  },
+};
