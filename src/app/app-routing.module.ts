ba98bb560ec8ce1f66daa1fed764af1b06111aaf
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2021-08-20 13:30:50 -0600

    Routing added for the new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,22 +8,40 @@ import { SearchIndividualComponent } from './component/page/individual/search-in
 import { UpdateIndividualComponent } from './component/page/individual/update-individual/update-individual.component';
 import { LandingComponent } from './component/page/landing/landing.component';
 import { LoginComponent } from './component/page/login/login.component';
+import { CreateProductComponent } from './component/page/product/create-product/create-product.component';
+import { SearchProductComponent } from './component/page/product/search-product/search-product.component';
+import { UpdateProductComponent } from './component/page/product/update-product/update-product.component';
 import { CreateUserComponent } from './component/page/user/create-user/create-user.component';
+import { MeasurementTypeResultsComponent } from './component/shared/measurementType/measurement-type-results/measurement-type-results.component';
+import { ProductBrandResultsComponent } from './component/shared/productBrand/product-brand-results/product-brand-results.component';
+import { ProductTypeResultsComponent } from './component/shared/productType/product-type-results/product-type-results.component';
 import { AppRoutes } from './utils/appRoutes';
 
 const routes: Routes = [
-  {path : AppRoutes.landing, component: LandingComponent},
-  {path: AppRoutes.login, component: LoginComponent, canActivate: [AuthGuard]},
-  {path: AppRoutes.user.create, component: CreateUserComponent},
-  {path: AppRoutes.individual.create, component: CreateIndividualComponent},
-  {path: AppRoutes.individual.update, component: UpdateIndividualComponent},
-  {path: AppRoutes.individual.search, component: SearchIndividualComponent},
-  {path: AppRoutes.error.internal, component: InternalErrorComponent},
-  {path: AppRoutes.error.notFound, component: NotFoundComponent} // 404 always goes at last.
+  { path: AppRoutes.landing, component: LandingComponent },
+  {
+    path: AppRoutes.login,
+    component: LoginComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: AppRoutes.user.create, component: CreateUserComponent },
+  { path: AppRoutes.individual.create, component: CreateIndividualComponent },
+  { path: AppRoutes.individual.update, component: UpdateIndividualComponent },
+  { path: AppRoutes.individual.search, component: SearchIndividualComponent },
+  { path: AppRoutes.product.create, component: CreateProductComponent },
+  { path: AppRoutes.product.search, component: SearchProductComponent },
+  { path: AppRoutes.product.update, component: UpdateProductComponent },
+
+  {
+    path: AppRoutes.measurementType.search,
+    component: ProductTypeResultsComponent,
+  },
+  { path: AppRoutes.error.internal, component: InternalErrorComponent },
+  { path: AppRoutes.error.notFound, component: NotFoundComponent }, // 404 always goes at last.
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation:'reload'})],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
