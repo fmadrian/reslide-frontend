624e4349b9dddeab227e2e4d506614449b4eeed0
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2021-09-16 16:11:28 -0600

    Order and order detail payload created

---
+++ src/app/payload/order/order.payload.ts
@@ -0,0 +1,16 @@
+import { OrderDetailPayload } from '../orderDetail/order-detail.payload';
+import { TransactionPayload } from '../transaction/transaction.payload';
+
+export interface OrderPayload {
+  id: number;
+  transaction: TransactionPayload;
+  providerName: string;
+  providerCode: string;
+  expectedDeliveryDate: string;
+  actualDeliveryDate: string;
+  total: number;
+  paid: number;
+  owed: number;
+  status: string;
+  details: OrderDetailPayload[];
+}
---
+++ src/app/payload/orderDetail/order-detail.payload.ts
@@ -0,0 +1,11 @@
+export interface OrderDetailPayload {
+  id: number;
+  orderId: number;
+  productCode: string;
+  productName: string;
+  priceByUnit: number;
+  quantity: number;
+  total: number;
+  status: string;
+  notes: string;
+}
