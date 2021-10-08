bbf57ad14f87dc5c8fee384fddacea9d97cbcc44
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2021-10-08 14:55:04 -0600

    Base component displays the sidebar and navbar if the user is logged in.
    Base component displays the sidebar and navbar if the user is logged in.

--- src/app/component/base/base.component.ts
+++ src/app/component/base/base.component.ts
@@ -1,4 +1,4 @@
-import { Output } from '@angular/core';
+import { EventEmitter, Input, Output } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { AuthService } from 'src/app/service/auth.service';
@@ -6,27 +6,32 @@ import { AuthService } from 'src/app/service/auth.service';
 @Component({
   selector: 'app-base',
   templateUrl: './base.component.html',
-  styleUrls: ['./base.component.scss']
+  styleUrls: ['./base.component.scss'],
 })
 export class BaseComponent implements OnInit {
-
-  isLoggedIn : boolean;
+  isLoggedIn: boolean = false;
+  username: string = '';
   // Sidebar
-  @Output() isSidenavOpen$ : BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true); // Behavior subject passed to the navbar component, so it can emit new values
-  isSidenavOpen : boolean;  // Value used to determine whether the sidenav has to be open. 
-  constructor(private authService: AuthService) { 
-    this.isLoggedIn = this.authService.isLoggedIn();
+  @Output() isSidenavOpen$: BehaviorSubject<boolean> =
+    new BehaviorSubject<boolean>(true); // Behavior subject passed to the navbar component, so it can emit new values
+  isSidenavOpen: boolean; // Value used to determine whether the sidenav has to be open.
+  constructor(private authService: AuthService) {
     this.isSidenavOpen = false;
+    this.username = this.authService.getUsername();
+    this.isLoggedIn = this.authService.isLoggedIn();
   }
 
   ngOnInit(): void {
     // 1. Subscribe the value to the behavior subject.
-   this.isSidenavOpen$.subscribe(
-     (data)=>{
+    // We are getting the event emitter defined in the auth service to know if the sidebar is open and if we are logged in.
+    this.isSidenavOpen$.subscribe((data) => {
       this.isSidenavOpen = data;
-     }
-   )
+    });
+    this.authService.username.subscribe((data) => {
+      this.username = this.authService.getUsername();
+    });
+    this.authService.loggedIn.subscribe((data) => {
+      this.isLoggedIn = this.authService.isLoggedIn();
+    });
   }
-  
 }
-
