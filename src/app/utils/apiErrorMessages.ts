57eaeca0aa497ad0a0a39165df7054287148e5f0
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2022-01-14 10:55:21 -0600

    Update apiErrorMessages.ts
    When it only receives an error, it returns it.

--- src/app/utils/apiErrorMessages.ts
+++ src/app/utils/apiErrorMessages.ts
@@ -5,24 +5,30 @@ export interface ApiError {
 export const ApiErrorMessage = (apiError: any) => {
   let translation: ApiError | null = null;
   if (apiError !== null && apiError !== undefined) {
-    let type = apiError.errorType;
     let input = '';
-    let message = apiError.message;
-
-    if (type === 'UsernameExistsException') {
-      input = 'username';
-    } else if (
-      type === 'IndividualCodeExistsException' ||
-      type === 'ProductExistsException'
-    ) {
-      input = 'code';
-    } else if (type === 'PriceNotValidException') {
-      input = 'price';
-    } else if (type === 'QuantityNotValidException') {
-      input = 'quantity';
+    let type = '';
+    let message = '';
+    // If we receive a complex object.
+    if (apiError.errorType || apiError.message) {
+      type = apiError.errorType;
+      message = apiError.message;
+      if (type === 'UsernameExistsException') {
+        input = 'username';
+      } else if (
+        type === 'IndividualCodeExistsException' ||
+        type === 'ProductExistsException'
+      ) {
+        input = 'code';
+      } else if (type === 'PriceNotValidException') {
+        input = 'price';
+      } else if (type === 'QuantityNotValidException') {
+        input = 'quantity';
+      } else {
+        message = 'Unknown error';
+        input = 'other';
+      }
     } else {
-      message = 'Unknown error';
-      input = 'other';
+      message = apiError;
     }
     translation = {
       message,
