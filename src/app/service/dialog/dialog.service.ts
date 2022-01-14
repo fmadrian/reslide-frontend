cccb4b887b2038d1cb9f8c09414c924a55c86fc7
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2022-01-14 10:48:58 -0600

    Update dialog.service.ts
    Dialog size changed, options to open measurement type, brand and product type dialogs added

--- src/app/service/dialog/dialog.service.ts
+++ src/app/service/dialog/dialog.service.ts
@@ -4,15 +4,18 @@ import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
 import { DiscountFormComponent } from 'src/app/component/shared/discount/discount-form/discount-form.component';
 import { IndividualResultsComponent } from 'src/app/component/shared/individual/individual-results/individual-results.component';
+import { MeasurementTypeResultsComponent } from 'src/app/component/shared/measurementType/measurement-type-results/measurement-type-results.component';
 import { ProductResultsComponent } from 'src/app/component/shared/product/product-results/product-results.component';
+import { ProductBrandResultsComponent } from 'src/app/component/shared/productBrand/product-brand-results/product-brand-results.component';
+import { ProductTypeResultsComponent } from 'src/app/component/shared/productType/product-type-results/product-type-results.component';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DialogService {
   dimensions = {
-    width: '600px',
-    height: '400px',
+    width: '65%',
+    height: '80%',
   };
   constructor(public dialog: MatDialog) {}
 
@@ -25,6 +28,14 @@ export class DialogService {
     } else if (name === 'product') {
       component = ProductResultsComponent;
     }
+    if (name === 'measurementType') {
+      component = MeasurementTypeResultsComponent;
+    } else if (name === 'brand') {
+      component = ProductBrandResultsComponent;
+    } else if (name === 'productType') {
+      component = ProductTypeResultsComponent;
+    }
+
     if (component) {
       let dialogRef: MatDialogRef<unknown, any>;
       // Add the data if there is any, open the component and return the result.
@@ -33,10 +44,15 @@ export class DialogService {
       } else {
         dialogRef = this.dialog.open(component, this.dimensions);
       }
-      /*dialogRef.afterClosed().subscribe((result) => {
-      
-      });*/
       return dialogRef.afterClosed();
+
+      /**
+       *  TO GET THE OBJECT OF THE DIALOG:
+       *
+       * this.dialogService.open('example').subscribe((result) => {
+       *     this.selectedValues.name.field = result?.field;
+       *  });
+       */
     }
     return null;
   }
