cdec70a5d884d6085fd0eb52b5725972390855cc
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2022-01-14 11:34:16 -0600

    Redirects to product after creating it
    Error while showing an error message on a snackbar, after creating a product redirects the user to the update page.

--- src/app/component/page/product/create-product/create-product.component.ts
+++ src/app/component/page/product/create-product/create-product.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { ProductPayload } from 'src/app/payload/product/product.payload';
 import { ProductService } from 'src/app/service/product/product.service';
 import { SnackbarService } from 'src/app/service/snackbar/snackbar.service';
 import { ApiError, ApiErrorMessage } from 'src/app/utils/apiErrorMessages';
+import { AppRoutes } from 'src/app/utils/appRoutes';
 
 @Component({
   selector: 'app-create-product',
@@ -14,19 +16,23 @@ export class CreateProductComponent implements OnInit {
   apiError: ApiError | null = null;
   constructor(
     private productService: ProductService,
-    private snackbarService: SnackbarService
+    private snackbarService: SnackbarService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {}
   create(product: ProductPayload) {
     this.productService.create(product).subscribe(
       (data) => {
-        this.snackbarService.show('Individual created');
+        this.snackbarService.show('Product created');
         this.apiError = null;
+        if (data.id) {
+          this.router.navigateByUrl(AppRoutes.product.update_id(data.id));
+        }
       },
       (error) => {
-        this.apiError = ApiErrorMessage(error.error);
-        this.snackbarService.show(error.error.message);
+        this.apiError = ApiErrorMessage(error);
+        this.snackbarService.show(error);
       }
     );
   }
